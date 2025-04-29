cdb4f632416d2c64c9d628d270124d886907c8b3
Paulo Bueno <paulob.bruno@gmail.com>
2025-04-28 23:29:32 -0300

    updating docker-compose

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,8 @@
+-- FAST API
 CREATE DATABASE portfolio CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'fastapi'@'%' IDENTIFIED BY 'fastapi123';
 GRANT ALL PRIVILEGES ON portfolio.* TO 'fastapi'@'%';
+-- AIRFLOW
 CREATE DATABASE airflow_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'airflow_user'@'%' IDENTIFIED BY 'airflow123';
 GRANT ALL PRIVILEGES ON airflow_db.* TO 'airflow_user'@'%';
